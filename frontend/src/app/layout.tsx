d26bbfa5b2120f1d55e08a0051beff2add748b20
Quân <otabe2703@gmail.com>
2025-01-10 15:45:41 +0700

    Feature(UI): Update layout and enhance product detail page with new components
    - Changed application title and description in layout.tsx to reflect project branding.
    - Redesigned SinglePage component to include ProductImages, CustomizeProducts, and Add components for improved product presentation and user interaction.
    - Introduced Add component for quantity selection and Add to Cart functionality.
    - Added CustomizeProducts component for color and size selection options.
    - Implemented ProductImages component to display product images with a gallery feature.
    - Updated NavIcons component to streamline cart interaction.
    
    These changes enhance the overall user experience and align the application with its new branding.

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -7,8 +7,8 @@ import Footer from "@/components/Footer";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Lama Dev E-Commerce Application",
-  description: "A complete e-commerce application with Next.js and Wix",
+  title: "ZETA",
+  description: "An online store project of AnhQuan27",
 };
 
 export default function RootLayout({
